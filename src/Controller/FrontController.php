2d721f2d6a318cd8586712a785763d6159cb938c
Erwann_Gallois <gallois.erwann@gmail.com>
2024-02-01 22:20:25 +0100

    Update dependencies and add contact form and add login

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        //Formulaire de contact
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $data = $form->getData();
+            dump($data);
+        }
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'form' => $form->createView()
         ]);
     }
+
 }
