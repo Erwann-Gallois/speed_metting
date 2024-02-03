4cae35719c69714005fb1954509429636b824265
Erwann_Gallois <gallois.erwann@gmail.com>
2024-02-03 19:40:44 +0100

    Mise à jour des fichiers et dépendances***
    Les fichiers et dépendances suivants ont été mis à jour :
    - templates/admin/index.html.twig : Le fichier a été modifié pour étendre le modèle 'base_back.html.twig' au lieu de 'base.html.twig'.
    - composer.json : La dépendance "symfony/google-mailer" a été ajoutée.
    - symfony.lock : La dépendance "symfony/google-mailer" a été ajoutée.
    - config/packages/messenger.yaml : Le routage pour "Symfony\Component\Mailer\Messenger\SendEmailMessage" a été commenté.
    - src/Controller/AdminController.php : L'annotation #[IsGranted('ROLE_ORGANISATEUR')] a été ajoutée.
    - templates/base_back.html.twig : Le lien "{{path ("acceuil")}}" a été modifié en "{{path ("accueil")}}".
    - config/packages/security.yaml : Les chemins de connexion et de déconnexion ont été modifiés.
    - .env : Le DSN pour le MAILER a été modifié pour utiliser Gmail.
    - templates/security/login.html.twig : La classe "custom-form" a été ajoutée au formulaire.
    - templates/registration/register.html.twig : La classe "custom-form" a été ajoutée au formulaire et le fichier "flash_message.html.twig" a été inclus.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin')]
+#[IsGranted('ROLE_ORGANISATEUR')]
 class AdminController extends AbstractController
 {
     #[Route('', name: 'admin')]
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Form\ContactFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
@@ -14,16 +16,49 @@ class FrontController extends AbstractController
     public function index(Request $request): Response
     {
         //Formulaire de contact
-        $form = $this->createForm(ContactFormType::class);
+        $form = $this->createForm(ContactFormType::class, null, ['method' => 'POST']);
         $form->handleRequest($request);
-        
         if ($form->isSubmitted() && $form->isValid()) { 
-            $data = $form->getData();
-            dump($data);
+            $contactFormData = $form->getData();
+            // $this->addFlash('success', 'Votre message a été envoyé');
+            return $this->redirectToRoute('mail_contact', [
+                'nom' => $contactFormData["nom"],
+                'email' => $contactFormData["email"],
+                'sujet' => $contactFormData["sujet"],
+                'message' => $contactFormData["message"]
+            ]);
         }
         return $this->render('front/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form
         ]);
+    }
+
+    #[Route('/contact/{nom}/{email}/{sujet}/{message}', name: 'mail_contact')]
+    public function contact(String $nom, String $email, String $sujet, String $message, MailerInterface $mailer): Response
+    {
+        dump($nom, $email, $sujet, $message);
+        $message = (new TemplatedEmail())
+            ->from($email)
+            ->to('lapatteprecieuse0@gmail.com')
+            ->subject('Contact - '.$sujet)
+            // path of the Twig template to render
+            ->htmlTemplate('mail/contact.html.twig')
+            // pass variables (name => value) to the template
+            ->context([
+                'nom' => $nom,
+                'adresse' => $email,
+                'sujet' => $sujet,
+                'message' => $message
+            ]);
+        $mailer->send($message);
+        $this->addFlash('success', 'Votre message a été envoyé');
+        return $this->redirectToRoute('accueil');
+    }
+
+    #[Route('/organisation', name: 'organisation')]
+    public function organisation(): Response
+    {
+        return $this->render('front/organisation.html.twig');
     }
 
 }
