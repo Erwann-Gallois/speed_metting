98d5c7b1e555b252332d89ed5ece94db46f37da1
Erwann_Gallois <gallois.erwann@gmail.com>
2024-02-05 06:18:29 +0100

    Ajouter des dépendances et des fonctionnalités, mettre à jour les templates et les contrôleurs, ajouter des fixtures pour les utilisateurs

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+// use Symfony\Component\CssSelector\XPath\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -11,11 +15,27 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_ORGANISATEUR')]
 class AdminController extends AbstractController
 {
+    public function __construct(
+        private ManagerRegistry $doctrine, 
+        private Security $security, 
+    )
+    {
+    }
     #[Route('', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/liste/professionnel', name: 'liste_professionnel')]
+    public function listeProfessionnel(): Response
+    {
+        $pro = $this->doctrine->getRepository(User::class)->findBy(['roles' => 'ROLE_PROFESSIONNEL', "type" => 1]);
+        return $this->render('admin/liste_professionnel.html.twig', [
+            'pros' => $pro,
+            // 'controller_name' => 'AdminController',
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+        $faker->addProvider(new \Brunty\Faker\BuzzwordJobProvider($faker));
+        //Creation Professionnel
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setNom($faker->lastName());
+            $user->setPrenom($faker->firstName());
+            $user->setEmail($faker->email());
+            $password = $this->hasher->hashPassword($user, '1234');
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_PROFESSIONNEL']);
+            $user->setType(1);
+            $user->setEntreprise($faker->company());
+            $user->setEtude($faker->text());
+            $user->setPoste($faker->jobTitle());
+            $user->setQuestion($faker->text());
+            $manager->persist($user);
+        }
+
+        //Creation Eleve
+        for ($i = 0; $i < 60; $i++) {
+            $user = new User();
+            $user->setNom($faker->lastName);
+            $user->setPrenom($faker->firstName);
+            $user->setEmail($faker->email);
+            $password = $this->hasher->hashPassword($user, '1234');
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_ELEVE']);
+            $user->setType(2);
+            $user->setFiliere($faker->text());
+            // $user->setEtude($faker->sentence(3));
+            // $user->setPoste($faker->jobTitle);
+            $user->setQuestion($faker->text());
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
