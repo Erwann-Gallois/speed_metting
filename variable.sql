eaf77c4954c449a3f06741c8815f0fa15954e7e6
Erwann-Gallois <gallois.erwann@gmail.com>
2024-05-10 12:38:02 +0200

    chore: Mettre à jour les variables de date et de places dans AdminController

--- variable.sql
+++ variable.sql
@@ -1,1 +1,1 @@
-INSERT INTO variable (id, date_fin_inscription, date_ouver_resa, date_fin_resa, place_session, place_rdv) VALUES (1, '2024-05-10 12:00:00', '2024-05-10 14:00:00', '2024-05-17 20:00:00', 30, 4);
+INSERT INTO speedmeetings.variable (id, date_fin_inscription, date_ouver_resa, date_fin_resa, place_session, place_rdv, place_rdv2) VALUES (1, '2024-05-10 12:00:00', '2024-05-10 14:00:00', '2024-05-17 20:00:00', 30, 4, 3);
